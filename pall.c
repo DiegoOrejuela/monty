90364ab94295f5838fbf53c2d5c05cd7901ac037
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-15 15:23:05 +0000

    betty

--- pall.c
+++ pall.c
@@ -1,16 +1,18 @@
 #include "monty.h"
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
- * @h: double linked list dlistint_t.
+ * pall - prints all the values on the stack, starting from the top
+ * of the stack.
+ * @stack: double linked list stack_t.
+ * @line_number: line of opcode in the file.m
  *
- * Return: amount of elements in linked list.
+ * Return: nothing.
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t i;
+	stack_t *i;
 	int j;
 	(void)line_number;
 
-	for (j = 0, i = *stack; i != NULL; j++, i = (*stack).next)
-		printf("%d\n", (*(*stack)).n);
+	for (j = 0, i = *stack; i != NULL; j++, i = (*i).next)
+		printf("%d\n", (*i).n);
 }
