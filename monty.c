54a8d1ea49128607e266e48ff8db603a1e65b6db
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-15 16:59:55 +0000

    correction betty

--- monty.c
+++ monty.c
@@ -8,14 +8,11 @@
  *
  * Return: 0 if it's success, and other value if not.
  */
-/* int n;*/
-/* void push(stack_t **stack, unsigned int line_number); */
 int main(int argc, char *argv[])
 {
 	instruction_t opcodes[] = {
-		{"push", push}/* , {"pall", pall}, {"pint", pint}, */
-		/* {"pop", pop}, {"swap", swap}, {"add", add}, */
-		/* {"nop", nop} */
+		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
+		{"swap", swap}, {"add", add}, {"nop", nop}
 	};
 
 	stack_t *head = NULL;
@@ -23,40 +20,46 @@ int main(int argc, char *argv[])
 	unsigned int counter = 1;
 	FILE *file;
 	ssize_t ch_read = 0;
-	char *buffer;
-	char *lines[2];
+	char *buffer, *lines[2];
 	size_t sizebuf = 0;
-	(void)argc;
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
-		return (1);
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
-	while(1)
+	while (1)
 	{
 		ch_read = getline(&buffer, &sizebuf, file);
 		if (ch_read == -1)
-			return(1);
+		{
+			free(buffer);
+			return (0);
+		}
 		lines[0] = strtok(buffer, " \n");
 		lines[1] = strtok(NULL, " \n");
-		for (i = 0; i < 1; i++)
+		for (i = 0; i < 4; i++)
 		{
-			if(strcmp(lines[0], opcodes[i].opcode) == 0)
+			if (strcmp(lines[0], opcodes[i].opcode) == 0)
 			{
-				n = atoi(lines[1]);
-				opcodes[i].f(&head, i);
+				if (lines[1] != NULL)
+					n = atoi(lines[1]);
+				opcodes[i].f(&head, counter);
 			}
-
+		}
+		if (i == 4)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", counter, argv[0]);
+			exit(EXIT_FAILURE);
 		}
 		counter++;
 	}
-	return(0);
+	return (0);
 }
-
-/* void push(stack_t **stack, unsigned int line_number) */
-/* { */
-/* 	(void)stack; */
-/* 	(void)line_number; */
-/* 	printf("Estamos en la función PUSH"); */
-/* } */
