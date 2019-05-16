6fffb37fbc55d1af57723d9849778bb64ef39305
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-16 23:31:10 +0000

    implement function rotl

--- monty.h
+++ monty.h
@@ -53,6 +53,8 @@ void sub(stack_t **stack, unsigned int line_number);
 void mul(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
+void rotl(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
+stack_t *add_end(stack_t **head, const int n);
 #endif /* MONTY_H */
