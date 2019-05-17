950d62e8614688c2fd4b1012bf757f92e0681b66
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-17 03:08:04 +0000

    move node to head

--- rotr.c
+++ rotr.c
@@ -10,17 +10,19 @@
 void rotr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *left;
-	int n_temp;
+	(void)line_number;
 
 	if (stack != NULL && *stack != NULL && (*(*stack)).next != NULL)
 	{
 		for (left = *stack; (*left).next != NULL; left = (*left).next)
 		{}
 
-		n_temp = (*left).n;
-
 		(*(*left).prev).next = NULL;
-		n[0] = convertd(n_temp);
-		push(stack, line_number);
+
+		(*left).prev = NULL;
+		(*left).next = *stack;
+		(*(*stack)).prev = left;
+
+		*stack = left;
 	}
 }
